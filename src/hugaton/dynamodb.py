dd0dea411066171c0c7b11a1971a0d1910a36b83
AlvarVG <alvar.viana@sysdig.com>
2021-12-01 15:35:16 +0100

    Create all the files to work on them

---
+++ src/hugaton/dynamodb.py
@@ -0,0 +1,6 @@
+def something():
+    # table_name = 'AlvarFirstTest'
+    # dynamodb = boto3.resource('dynamodb')
+    # table = dynamodb.Table(table_name)
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html
+    pass
