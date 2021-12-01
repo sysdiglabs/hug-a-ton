7120a845ee0273969b82d82c154513ddf6dc8c84
AlvarVG <alvar.viana@sysdig.com>
2021-12-01 17:53:08 +0100

    Trying to give commands

--- src/hugaton/command_give.py
+++ src/hugaton/command_give.py
@@ -1,6 +1,34 @@
+from dynamodb import something
+from slack import get_userid_from_user
+from slack import something
+from command_balance import get_hug_available
+
 def command_give(sender, receiver, message):
+
+    if check_sender_receiver(sender, receiver):
+        return {
+            'statusCode': 200,
+            'headers': {'Content-type': 'application/json'},
+            'body': ":no_entry_sign: You can not send shovels to yourself"
+        }
+
+    if check_hug_available(sender):
+        return {
+            'statusCode': 200,
+            'headers': {'Content-type': 'application/json'},
+            'body': "You ran out of hugs"
+        }
+
     return {
         'statusCode': 200,
         'headers': {'Content-type': 'application/json'},
-        'body': f"give {sender} {receiver} {message}"
-    }
+        'body': "Sended"
+    }   
+
+def check_sender_receiver(sender, receiver):
+    return sender != get_userid_from_user(receiver)
+
+def check_hug_available(sender):
+    return False
+
+
--- src/hugaton/slack.py
+++ src/hugaton/slack.py
@@ -1,2 +1,6 @@
 def something():
     pass
+
+def get_userid_from_user(user):
+    #<@id|nombre>
+    return user.split('|')[0].split('@')[1]
