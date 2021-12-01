f4b710db23cbadc725ce4dd84a9e1fa973dcc5de
Jorge Maroto <jmaroto@gmail.com>
2021-12-01 19:02:54 +0100

    balance fixes

--- src/hugaton/command_balance.py
+++ src/hugaton/command_balance.py
@@ -1,7 +1,8 @@
 import dynamodb
 import json
 
-def command_balance(params, body):
+
+def command_balance(_, body):
     hugs_available = dynamodb.hugs_available(body['user_id'], body['user_name'])
     hugs_received = dynamodb.hugs_received(body['user_id'], body['user_name'])
 
@@ -28,12 +29,16 @@ def format_message(hugs_available, hugs_received):
 
 
 def hugs_available_block(hugs):
+    if hugs == 1:
+        hugs_text = f"{hugs} hug"
+    else:
+        hugs_text = f"{hugs} hugs"
     return [
         {
             "type": "section",
             "text": {
                 "type": "mrkdwn",
-                "text": f":hugging_face: {hugs} hugs available*"
+                "text": f":hugging_face: *{hugs_text} available*"
             }
         },
         {
@@ -66,46 +71,42 @@ def no_hugs_available_block():
 
 
 def hugs_received_block(hugs):
+    if hugs == 1:
+        hugs_text = f"{hugs} hug"
+    else:
+        hugs_text = f"{hugs} hugs"
     return [
         {
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f":sysdig_party: *{hugs} hugs available*"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Your teammates are recognizing your contributions! Keep up the great work!"
-                    }
-                }
-            ]
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f":sysdig_party: *{hugs_text} received*"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Your teammates are recognizing your contributions! Keep up the great work!"
+            }
         }
-]
+    ]
 
 
 def no_hugs_received_block():
     return [
         {
-            "blocks": [
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": f":sad_panda: *No hugs received yet!*"
-                    }
-                },
-                {
-                    "type": "section",
-                    "text": {
-                        "type": "mrkdwn",
-                        "text": "Don't give up! Keep trying, soon your effort will be recognized!"
-                    }
-                }
-            ]
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f":sad_panda: *No hugs received yet*"
+            }
+        },
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Don't give up! Keep trying, soon your effort will be recognized!"
+            }
         }
-]
+    ]
