32116bf9df5e13cbe3d6401449709ef24b1491d7
AlvarVG <alvar.viana@sysdig.com>
2021-12-01 19:11:54 +0100

    Fix problems

--- src/hugaton/dynamodb.py
+++ src/hugaton/dynamodb.py
@@ -1,12 +1,26 @@
 def hugs_available(user_id, user_name):
     # TODO
-    return 1
+    return 0
 
 
 def hugs_received(user_id, user_name):
     # TODO
     return 2
 
+
 def give_hug(sender_id, receiver_id, message):
     # TODO
     pass
+
+
+###### Doc schema
+#{
+#  timestamp: string
+#  sender_name: string
+#  sender_id: string
+#  receiver_name: string
+#  receiver_id: string
+#  message: string
+#  spent: boolean
+#}
+########################
