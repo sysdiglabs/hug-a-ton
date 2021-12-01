e15dbd490a1d325c8f70e5efec7ef3ac682dba05
Javier Celaya <javier.celaya@sysdig.com>
2021-12-01 16:19:48 +0100

    Use black to format Python code

--- src/hugaton/command_balance.py
+++ src/hugaton/command_balance.py
@@ -1,6 +1,6 @@
 def command_balance(command):
     return {
-        'statusCode': 200,
-        'headers': {'Content-type': 'application/json'},
-        'body': "20 hubs available"
+        "statusCode": 200,
+        "headers": {"Content-type": "application/json"},
+        "body": "20 hubs available",
     }
--- src/hugaton/command_give.py
+++ src/hugaton/command_give.py
@@ -1,7 +1,6 @@
 def command_give(command):
     return {
-        'statusCode': 200,
-        'headers': {'Content-type': 'application/json'},
-        'body': f"give {command} {type(command)} {command.split()}"
-    }  
- 
+        "statusCode": 200,
+        "headers": {"Content-type": "application/json"},
+        "body": f"give {command} {type(command)} {command.split()}",
+    }
