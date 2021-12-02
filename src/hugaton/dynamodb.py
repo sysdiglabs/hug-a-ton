f63815c35213c4d481c57b944dc87ed3831840a6
AlvarVG <alvar.viana@sysdig.com>
2021-12-02 13:00:47 +0100

    Donate command

--- src/hugaton/dynamodb.py
+++ src/hugaton/dynamodb.py
@@ -1,7 +1,7 @@
 import boto3
 import os
 import datetime
-import pprint
+import uuid
 from boto3.dynamodb.conditions import Key
 from boto3.dynamodb.conditions import Attr
 
@@ -31,10 +31,10 @@ from boto3.dynamodb.conditions import Attr
 # https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/GettingStarted.Python.03.html
 # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/dynamodb.html
 dynamodb = boto3.resource("dynamodb")
-table = dynamodb.Table(os.getenv("DYNAMODB_TABLE", "aws_dynamodb_table.hugs.name"))
 
 
 def hugs_available(user_id, user_name):
+    table = dynamodb.Table(os.getenv("DYNAMODB_TABLE", "aws_dynamodb_table.hugs.name"))
     response = table.query(KeyConditionExpression=Key("sender_id").eq(user_id))
     # TODO: Filter this in DB to improve performance
     hugs_given_this_month = [
@@ -48,7 +48,8 @@ def hugs_available(user_id, user_name):
 
 
 def hugs_received(user_id, user_name):
-    # TODO: FAdd index into "receiver_id" and change scan for query
+    table = dynamodb.Table(os.getenv("DYNAMODB_TABLE", "aws_dynamodb_table.hugs.name"))
+    # TODO: Add index into "receiver_id" and change scan for query
     scan_kwargs = {
         "FilterExpression": Key("receiver_id").eq(user_id),
         "ProjectionExpression": "receiver_id, spent",
@@ -71,6 +72,9 @@ def hugs_received(user_id, user_name):
 
 
 def give_hug(sender_id, sender_name, receiver_id, receiver_name, message):
+    table = dynamodb.Table(
+        os.getenv("DYNAMODB_HUG_TABLE", "aws_dynamodb_table.hugs.name")
+    )
     table.put_item(
         Item={
             "timestamp": str(datetime.datetime.utcnow()),
@@ -93,5 +97,50 @@ def is_hug_spent(hug):
     return hug["spent"]
 
 
-def update_hugs(user_id, user_name, num_hugs):
-    return
+def donate(donator_id, donator_name, amount, charity_link):
+    table = dynamodb.Table(
+        os.getenv("DYNAMODB_DONATIONS_TABLE", "aws_dynamodb_table.donations.name")
+    )
+    transaction_id = str(uuid.uuid1())
+    table.put_item(
+        Item={
+            "transaction_id": transaction_id,
+            "timestamp": str(datetime.datetime.utcnow()),
+            "donator_name": donator_name,
+            "donator_id": donator_id,
+            "amount": amount,
+            "destination": charity_link,
+        }
+    )
+    mark_received_hugs_as_spent(donator_id, donator_name, amount)
+    return transaction_id
+
+
+def mark_received_hugs_as_spent(receiver_id, receiver_name, amount):
+    table = dynamodb.Table(
+        os.getenv("DYNAMODB_HUGS_TABLE", "aws_dynamodb_table.hugs.name")
+    )
+    # TODO: Add index into "receiver_id" and change scan for query
+    scan_kwargs = {
+        "FilterExpression": Key("receiver_id").eq(receiver_id),
+    }
+
+    hugs_spent = 0
+    start_key = None
+
+    while hugs_spent < amount:
+        if start_key:
+            scan_kwargs["ExclusiveStartKey"] = start_key
+        response = table.scan(**scan_kwargs)
+        hugs_to_spend = [item for item in response["Items"] if not is_hug_spent(item)]
+        for hug in hugs_to_spend:
+            if hugs_spent > amount:
+                return
+            table.update_item(
+                Key={"sender_id": hug["sender_id"], "timestamp": hug["timestamp"]},
+                UpdateExpression="SET spent = :val1",
+                ExpressionAttributeValues={":val1": True},
+            )
+            hugs_spent = +1
+
+        start_key = response.get("LastEvaluatedKey", None)
