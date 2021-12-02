0402e44883fbc6ebcc62236d5755ae52f4258342
Andrés Fuentes <andres.fuentes@sysdig.com>
2021-12-02 11:51:14 +0100

    Fix hug message

--- src/hugaton/slack.py
+++ src/hugaton/slack.py
@@ -3,6 +3,19 @@ import urllib3
 import emojis
 
 
+def format_text(receiver, message):
+    return [
+        {
+            "type": "section",
+            "text": {"type": "mrkdwn", "text": f":hugging_face: {receiver} got hugged"},
+        },
+        {
+            "type": "section",
+            "text": {"type": "mrkdwn", "text": f"{message.capitalize()}"},
+        },
+    ]
+
+
 def get_user_info(user):
     # <@id|nombre>
     id, name = user.split("|")
@@ -18,9 +31,9 @@ def notify_hug_in_channel(receiver, message):
 def notify(channel_id, message):
     url = "https://slack.com/api/chat.postMessage"
     token = os.environ["SLACK_TOKEN"]
-    fields = {
-        "channel": channel_id,
-        "text": message,
+    slack_fields = {
+        "channel": os.environ["SLACK_CHANNEL_ID"],
+        "blocks": json.dumps(format_text(receiver, message)),
     }
     http = urllib3.PoolManager()
     response = http.request(
@@ -30,6 +43,6 @@ def notify(channel_id, message):
             "Content-type": "application/json",
             "Authorization": f"Bearer {token}",
         },
-        fields=fields,
+        fields=slack_fields,
     )
     return response
