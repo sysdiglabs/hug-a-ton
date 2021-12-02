33fd0374033e19032921e4065fa18faa20b65a03
Javier Celaya <javier.celaya@sysdig.com>
2021-12-02 12:17:15 +0100

    Use a tfvars file instead of SOPS

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,11 +1,7 @@
 locals {
   prefix      = terraform.workspace == "default" ? "hug-a-ton" : terraform.workspace
-  domain_name = "dev.draios.com"
+  domain_name = var.domain_name
   subdomain   = local.prefix
-  cert_arn    = data.sops_file.secrets.data.cert_arn # *.dev.draios.com
-  slack_token = data.sops_file.secrets.data.slack_token
-}
-
-data "sops_file" "secrets" {
-  source_file = "secrets.json"
+  cert_arn    = sensitive(var.cert_arn)
+  slack_token = sensitive(var.slack_token)
 }
